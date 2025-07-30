7071bf798a9c8b82c81ed4d39bca5657d8d3f01b
shekhrbhardwaj <shekhrbhardwaj@gmail.com>
2025-07-30 10:04:04 +0530

    Refactor data fetching in dashboard page to consolidate API calls and improve readability

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,21 +2,18 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices, fetchTotalPaidInvoice, fetchTotalPendingInvoice, fetchTotalInvoices, fetchTotalCustomer } from '../lib/data';
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data';
 export default async function Page() {
     const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
-    const totalPaidInvoices = await fetchTotalPaidInvoice();
-    const totalPendingInvoices = await fetchTotalPendingInvoice();
-    const numberOfInvoices = await fetchTotalInvoices();
-    const numberOfCustomers = await fetchTotalCustomer();
+    const {numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices} = await fetchCardData();
     return (
         <main>
         <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
             Dashboard
         </h1>
         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-            <Card title="Collected" value={Number(totalPaidInvoices.total_invoices).toFixed(2)} type="collected" />
+            <Card title="Collected" value={Number(totalPaidInvoices).toFixed(2)} type="collected" />
             <Card title="Pending" value={Number(totalPendingInvoices).toFixed(2)} type="pending" />
             <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
             <Card
